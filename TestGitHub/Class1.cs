8992785f0e73622196afab0de42390767eb665a2
liqiangtq <liqiang@tq-soft.com>
2014-03-04 14:08:31 +0800

    asa

--- TestGitHub/Class1.cs
+++ TestGitHub/Class1.cs
@@ -63,8 +63,7 @@ namespace TestGitHub
         private int LiuhaoTest()
         {
             //liuhao add 2014-02-17
-            //return 0;
-			return 11;
+            return 0;
         }
     }
 }
